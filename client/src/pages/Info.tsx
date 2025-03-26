a5173c9fe01011c838e675f0118290c1073ca0fa
Shalom Quek <shalomquek@gmail.com>
2025-03-26 19:29:16 +0800

    Testing Cursor Git plugin

--- client/src/pages/Info.tsx
+++ client/src/pages/Info.tsx
@@ -34,7 +34,7 @@ function createData(
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Yellow Tails", 159, 6.0, 24, 4.0),
   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
